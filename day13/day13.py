01526db54d63fc23f81169a1367b9badd3e0b0d6
Alex Russell <ajex94@gmail.com>
2020-12-21 23:56:15 -0600

    Day13 Part 2

--- day13/day13.py
+++ day13/day13.py
@@ -3,7 +3,6 @@ import userin
 def parseInput(s):
     (time, arr) = s.splitlines()
     arr = arr.split(",")
-    arr = [int(x) for x in arr if x != "x"]
     return (int(time), arr)
 
 def nearestBus(cur, buses):
@@ -13,7 +12,32 @@ def nearestBus(cur, buses):
             if cur % b == 0:
                 return (cur, b)
 
-(curTime, busIDs) = parseInput(userin.default)
+# Part 1
+(curTime, busOrder) = parseInput(userin.default)
+busIDs = [int(x) for x in busOrder if x != "x"]
 (t, b) = nearestBus(curTime, busIDs)
 part1 = (t - curTime) * b
 print(part1)
+
+userin_arr = userin.default.splitlines()
+start = userin_arr[0]
+busses = ["x" if x == "x" else int(x) for x in userin_arr[1].split(",")]
+def part2():
+    # Reduce to mods
+    mods = {bus: -i % bus for i, bus in enumerate(busses) if bus != "x"}
+    vals = list(reversed(sorted(mods)))
+    val = mods[vals[0]]
+    # first point
+    # 
+    r = vals[0]
+    for b in vals[1:]:
+        while val % b != mods[b]:
+            val += r
+        r *= b
+    return val
+print(part2())
+
+# once an order is found the repitition pattern appears
+# n = 12345678901234567890
+# 1 = 0 0 0 0 1 0 0 0 0 1 
+# 5 =     0    1    0    1
